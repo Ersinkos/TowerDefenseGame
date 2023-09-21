09d51a0afd526291f8e97a0d098f7481c9ce7700
Ersin Köseoğlu <ersinkoseoglu38@gmail.com>
2023-09-21 12:54:28 +0300

    Shooting and Particle Effect

--- Tower Defense/Assets/Scripts/Turret.cs
+++ Tower Defense/Assets/Scripts/Turret.cs
@@ -5,12 +5,20 @@ using UnityEngine;
 public class Turret : MonoBehaviour
 {
     private Transform target;
-    public Transform partToRotate;
 
+    [Header("Attributes")]
     public float range = 15f;
-    public float turnSpeed = 10f;
+    public float fireRate = 1f;
+    private float fireCountdown = 0f;
 
+    [Header("Unity Setup Field")]
+    public Transform partToRotate;
+    public float turnSpeed = 10f;
     public string enemyTag = "Enemy";
+
+    public GameObject bulletPrefab;
+    public Transform firePoint;
+
 
     private void Start()
     {
@@ -42,7 +50,7 @@ public class Turret : MonoBehaviour
     }
     private void Update()
     {
-        if (target == null) return;
+        if (target == null) return; //if there is no target return.
 
         //Target lock on
         Vector3 dir = target.position - transform.position;
@@ -50,6 +58,20 @@ public class Turret : MonoBehaviour
         Vector3 rotation = Quaternion.Lerp(partToRotate.rotation, lookRotation, Time.deltaTime * turnSpeed).eulerAngles;//lerp for smooth turn
         partToRotate.rotation = Quaternion.Euler(0f, rotation.y, 0f);
 
+        if (fireCountdown <= 0)
+        {
+            Shoot();
+            fireCountdown = 1 / fireRate;
+        }
+        fireCountdown -= Time.deltaTime;
+
+    }
+    void Shoot()
+    {
+        GameObject bulletGO = (GameObject)Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        Bullet bullet = bulletGO.GetComponent<Bullet>();
+
+        if (bullet != null) bullet.Seek(target);
 
     }
     private void OnDrawGizmosSelected()//Turret range drawn when it's selected.
